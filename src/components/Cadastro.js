01acbba218c7adb8aacc10c83a160dfcacc53238
Augusto Ferrari <ferrariaugusto290975@gmail.com>
2025-09-30 22:20:01 -0300

    commit 71

--- src/components/Cadastro.js
+++ src/components/Cadastro.js
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getUsuarios, salvarUsuario } from "../utils/storage";
-import "App.css";
+import "./App.css";
 
 const Cadastro = () => {
   const [email, setEmail] = useState("");
--- src/components/TabelaProdutos.js
+++ src/components/TabelaProdutos.js
@@ -1,6 +1,6 @@
 // src/components/TabelaProdutos.js
 import React, { useEffect, useState } from "react";
-import "App.css";
+import "./App.css";
 import {
   getProdutos,
   salvarProduto,
